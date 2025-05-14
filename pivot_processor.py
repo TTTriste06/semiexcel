4656bb39fcfc352038fe25edabf04ac16970562b
TTTriste06 <yunhan.yang08@gmail.com>
2025-05-14 15:23:48 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -107,11 +107,13 @@ class PivotProcessor:
                             st.success("✅ 已合并预测数据")
 
                             # 追加成品库存信息
+                            df_finished = apply_mapping_and_merge(df_finished)
                             st.write(df_finished)
                             summary_preview = merge_finished_inventory(summary_preview, df_finished)
                             st.success("✅ 已合并成品库存")
 
                             # 追加成品在制信息
+                            product_in_progress = apply_mapping_and_merge(product_in_progress)
                             st.write(product_in_progress)
                             summary_preview = append_product_in_progress(summary_preview, product_in_progress, mapping_df)
                             st.success("✅ 已合并成品在制")
