b75f486ab9cfab86c705a2fc48661c373be31799
TTTriste06 <yunhan.yang08@gmail.com>
2025-05-19 15:22:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,34 +4,34 @@ CONFIG = {
     "input_dir": r"D:\运营数据\原始数据",
     "output_file": r"D:\运营数据\Report\运营数据订单-在制-库存汇总报告_{}.xlsx".format(datetime.now().strftime("%Y%m%d_%H%M%S")),
     "pivot_config": {
-        "weijiao.xlsx": {
+        "赛卓-未交订单.xlsx": {
             "index": ["晶圆品名", "规格", "品名"],
             "columns": "预交货日",
             "values": ["订单数量", "未交订单数量"],
             "aggfunc": "sum",
             "date_format": "%Y-%m"
         },
-        "zaizhi.xlsx": {
+        "赛卓-成品在制.xlsx": {
             "index": ["工作中心", "封装形式", "晶圆型号", "产品规格", "产品品名"],
             "columns": "预计完工日期",
             "values": ["未交"],
             "aggfunc": "sum",
             "date_format": "%Y-%m"
         },
-        "cp.xlsx": {
+        "赛卓-CP在制.xlsx": {
             "index": ["晶圆型号", "产品品名"],
             "columns": "预计完工日期",
             "values": ["未交"],
             "aggfunc": "sum",
             "date_format": "%Y-%m"
         },
-        "kucun.xlsx": {
+        "赛卓-成品库存.xlsx": {
             "index": ["WAFER品名", "规格", "品名"],
             "columns": "仓库名称",
             "values": ["数量"],
             "aggfunc": "sum"
         },
-        "yuan.xlsx": {
+        "赛卓-晶圆库存.xlsx": {
             "index": ["WAFER品名", "规格"],
             "columns": "仓库名称",
             "values": ["数量"],
